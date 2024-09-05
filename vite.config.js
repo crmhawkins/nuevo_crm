f84af176e8c9b5329a38c677739fb844f7ba79c0
DesarrolloHawkins <122017573+DesarrolloHawkins@users.noreply.github.com>
2024-09-05 09:48:41 +0200

    vite

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,22 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
-      laravel({
-        input: ['resources/css/app.css', 'resources/js/app.js'],
-        refresh: true,
-      }),
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
     ],
-    base: '/', // Asegúrate de que sea la ruta absoluta
+    base: '/', // Asegúrate de que el valor base sea correcto para producción
     build: {
-      outDir: 'public/build', // Ruta de salida donde se genera la build
+        outDir: 'public/build',
+        manifest: true, // Genera un manifest.json para que Laravel gestione los archivos correctamente
+        rollupOptions: {
+            output: {
+                entryFileNames: 'assets/[name].[hash].js',
+                chunkFileNames: 'assets/[name].[hash].js',
+                assetFileNames: 'assets/[name].[hash].[ext]',
+            },
+        },
     },
-  });
-  
+});
+
