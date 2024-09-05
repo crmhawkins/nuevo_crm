1f80c2587b90a86c4ea7cd3264bc85a0e6c0e6cf
DesarrolloHawkins <122017573+DesarrolloHawkins@users.noreply.github.com>
2024-09-05 09:43:43 +0200

    vite

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,14 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
-        laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
+      laravel({
+        input: ['resources/css/app.css', 'resources/js/app.js'],
+        refresh: true,
+      }),
     ],
-});
+    base: '/', // Asegúrate de que sea la ruta absoluta
+    build: {
+      outDir: 'public/build', // Ruta de salida donde se genera la build
+    },
+  });
+  
