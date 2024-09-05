9a89ab37cce427eb3936613be3eae801fed7d97a
DesarrolloHawkins <122017573+DesarrolloHawkins@users.noreply.github.com>
2024-09-05 10:11:39 +0200

    coomit vite

--- vite.config.js
+++ vite.config.js
@@ -4,21 +4,11 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
-    base: '/', // Asegúrate de que el valor base sea correcto para producción
-    build: {
-        outDir: 'public/build',
-        manifest: true, // Genera un manifest.json para que Laravel gestione los archivos correctamente
-        rollupOptions: {
-            output: {
-                entryFileNames: 'assets/[name].[hash].js',
-                chunkFileNames: 'assets/[name].[hash].js',
-                assetFileNames: 'assets/[name].[hash].[ext]',
-            },
-        },
-    },
 });
-
