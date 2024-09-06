d8aba166cdbff82a9a10814bc59909dc75429b65
crmhawkins <128600888+crmhawkins@users.noreply.github.com>
2024-09-06 09:35:15 +0200

    Correccion en edit facturas

--- resources/views/invoices/edit.blade.php
+++ resources/views/invoices/edit.blade.php
@@ -46,13 +46,13 @@
                                     <div class="col-md-6 col-sm-12">
                                         <div class="form-group mb-3">
                                             <label class="text-left mb-2">Presupuesto <span id="budgetLinkContainer"></span><a id="budgetAssignedClient" target="_blank" href="{{ route('presupuesto.edit', $factura->budget_id) }}"><i class="fas fa-external-link-alt"></i></a></label>
-                                            <input type="text" class="form-control @error('budget_id') is-invalid @enderror" id="budget_id" value="{{ $factura->budget->name ??  ($factura->client_id ? 'Cliente borrado' : 'Sin presupuesto asignado') }}" name="budget_id" disabled>
+                                            <input type="text" class="form-control @error('budget_id') is-invalid @enderror" id="budget_id" value="{{ $factura->budget->concept ??  ($factura->budget_id ? 'Presupuesto borrado' : 'Sin presupuesto asignado') }}" name="budget_id" disabled>
                                         </div>
                                     </div>
                                     <div class="col-md-6 col-sm-12">
                                         <div class="form-group mb-3">
                                             <label class="text-left mb-2">Cliente <span id="budgetLinkContainer"></span><a id="budgetAssignedClient" target="_blank" href="{{ route('clientes.edit', $factura->client_id) }}"><i class="fas fa-external-link-alt"></i></a></label>
-                                            <input type="text" class="form-control @error('client_id') is-invalid @enderror" id="client_id" value="{{ $factura->client->name ??  ($factura->client_id ? 'Cliente borrado' : 'Sin cliente asignado') }}" name="client_id" disabled>
+                                            <input type="text" class="form-control @error('client_id') is-invalid @enderror" id="client_id" value="{{ $factura->cliente->name ??  ($factura->client_id ? 'Cliente borrado' : 'Sin cliente asignado') }}" name="client_id" disabled>
                                         </div>
                                     </div>
                                     <div class="col-md-6 col-sm-12">
