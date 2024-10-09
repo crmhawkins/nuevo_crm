969ae034b70300fe043d6362e27c1f9bb292d98f
crmhawkins <128600888+crmhawkins@users.noreply.github.com>
2024-10-09 11:40:35 +0200

    Update clasificacion.blade.php

--- resources/views/logs/clasificacion.blade.php
+++ resources/views/logs/clasificacion.blade.php
@@ -15,15 +15,18 @@
 
     <section class="section mt-4">
         <!-- Pestañas -->
-        <ul class="nav nav-tabs" id="userTabs" role="tablist">
-            @foreach ($clasificacion as $usuario => $cambios)
-                <li class="nav-item" role="presentation">
-                    <a class="nav-link {{ $loop->first ? 'active' : '' }}" id="tab-{{ $usuario }}-tab" data-bs-toggle="tab" href="#tab-{{ $usuario }}" role="tab" aria-controls="tab-{{ $usuario }}" aria-selected="{{ $loop->first ? 'true' : 'false' }}">
-                        {{ $usuarios[$usuario]->name ?? 'Usuario Desconocido' }}
-                    </a>
-                </li>
-            @endforeach
-        </ul>
+        <div class="card-body">
+            <ul class="nav nav-tabs" id="userTabs" role="tablist">
+                @foreach ($clasificacion as $usuario => $cambios)
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link {{ $loop->first ? 'active' : '' }}" id="tab-{{ $usuario }}-tab" data-bs-toggle="tab" href="#tab-{{ $usuario }}" role="tab" aria-controls="tab-{{ $usuario }}" aria-selected="{{ $loop->first ? 'true' : 'false' }}">
+                            {{ $usuarios[$usuario]->name ?? 'Usuario Desconocido' }}
+                        </a>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+
 
         <div class="tab-content mt-3">
             @foreach ($clasificacion as $usuario => $cambios)
