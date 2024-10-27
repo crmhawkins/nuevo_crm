65b63a31ccb38f7e957cce77b2350f1f85ff83f3
DesarrolloHawkins <122017573+DesarrolloHawkins@users.noreply.github.com>
2024-10-27 21:00:29 +0100

    eerror dashboard controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -141,7 +141,7 @@ class DashboardController extends Controller
                         $productividadTotal += $productividad;
                     }
                     $productividadIndividual = $totalTareas > 0 ? $totalProductividad : 0;
-                
+                }
                     // Promedio de productividad
                     //echo "La productividad es: " . ($totalTareas > 0 ? $totalProductividad : 0) . "%";
                     
