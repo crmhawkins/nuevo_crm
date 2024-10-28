80ce72093b9ee11071a250448ab285414267f0c2
crmhawkins <128600888+crmhawkins@users.noreply.github.com>
2024-10-28 16:14:21 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -156,7 +156,7 @@ Route::get('/bajas', [BajaController::class, 'index'])->name('bajas.index');
 Route::get('/baja/create', [BajaController::class, 'create'])->name('bajas.create');
 Route::get('/baja/edit/{id}', [BajaController::class, 'edit'])->name('bajas.edit');
 Route::post('/bajas/store', [BajaController::class, 'store'])->name('bajas.store');
-Route::post('/bajas/update/{baja}', [BajaController::class, 'update'])->name('bajas.update');
+Route::post('/bajas/update/{id}', [BajaController::class, 'update'])->name('bajas.update');
 Route::post('/bajas/delete', [BajaController::class, 'destroy'])->name('bajas.delete');
 
 
