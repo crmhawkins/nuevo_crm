194bec6d65d5c7ded60fdd1b50be5b07a2474149
crmhawkins <128600888+crmhawkins@users.noreply.github.com>
2024-10-30 14:02:43 +0100

    Update index.blade.php

--- resources/views/statistics/index.blade.php
+++ resources/views/statistics/index.blade.php
@@ -60,11 +60,6 @@
                                         <div>
                                             <span class="d-block display-6 font-weight-400 text-dark">{{$dataBudgets['total']}}+</span>
                                         </div>
-                                        <div>
-                                            <span id="pie_chart_1" class="d-flex easy-pie-chart" data-percent="{{$dataBudgets['total']}}">
-                                                <span class="percent head-font">{{$dataBudgets['total']}}</span>
-                                            </span>
-                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -76,12 +71,7 @@
                                     <span class="d-block font-11 font-weight-500 text-dark text-uppercase mb-10">Presupuestos</span>
                                     <div class="d-flex align-items-end justify-content-between">
                                         <div>
-                                            <span class="d-block display-6 font-weight-400 text-dark">€ <span class="counter-anim">{{$countTotalBudgets}}</span></span>
-                                        </div>
-                                        <div >
-                                            <span id="pie_chart_2" class="d-flex easy-pie-chart" data-percent="75">
-                                                <span class="percent head-font">{{$countTotalBudgets}}</span>
-                                            </span>
+                                            <span class="d-block display-6 font-weight-400 text-dark"><span class="counter-anim">{{$countTotalBudgets}}</span> €</span>
                                         </div>
                                     </div>
                                 </div>
@@ -296,6 +286,7 @@
             }
         });
 
+        console.log(@json($allArray));
         // Facturación Anual
         var ctx2 = document.getElementById("facturacion-all-monthly").getContext("2d");
         var myChart2 = new Chart(ctx2, {
