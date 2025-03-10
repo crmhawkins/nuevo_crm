6fe86c02856ba2ca0e380ecf5be24674c0cfe417
crmhawkins <128600888+crmhawkins@users.noreply.github.com>
2025-03-10 16:16:39 +0100

    Update timeLine.blade.php

--- resources/views/horas/timeLine.blade.php
+++ resources/views/horas/timeLine.blade.php
@@ -63,7 +63,7 @@
             slotLabelFormat: 'HH:mm', // Formato de hora 24h (puedes usar 'h:mm A' para AM/PM)
             minTime: "07:00:00", // Muestra desde las 6 AM
             maxTime: "23:00:00", // Muestra hasta las 10 PM
-            slotDuration: "00:15:00", // Intervalo de media hora entre slots
+            slotDuration: "00:10:00", // Intervalo de media hora entre slots
 
         });
     });
