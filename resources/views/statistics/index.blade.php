30862fe5a2f4b5c1e6a5dcd4c11ae698d8df66a0
crmhawkins <128600888+crmhawkins@users.noreply.github.com>
2025-03-14 10:23:29 +0100

    Update index.blade.php

--- resources/views/statistics/index.blade.php
+++ resources/views/statistics/index.blade.php
@@ -74,18 +74,18 @@
                     <div class="row">
                         <form action="{{ route('estadistica.index') }}" method="GET">
                             <div class="row">
-                                <div class="col-12">
-                                    <div class="form-row align-items-end">
-                                        <div class="form-group col-md-5">
-                                            <label for="fecha_inicio" class="font-weight-bold">F.Inicio</label>
-                                            <input type="date" name="fecha_inicio" id="fecha_inicio" class="form-control" value="{{ request('fecha_inicio', $fecha_inicio ?? now()->format('Y-m-d')) }}">
+                                <div class="col-xl-12">
+                                    <div class="form-group mx-3 mb-3" style="display: flex; flex-direction: row; align-items: baseline;">
+                                        <div class="form-group mx-2 col-5">
+                                            <label class="form-label" for="mes"><strong>F.Inicio</strong></label>
+                                            <input class="form-control" type="date" name="fecha_inicio" id="fecha_inicio" class="form-control" style="margin-right: 1rem;" value="{{ request('fecha_inicio', $fecha_inicio ?? now()->format('Y-m-d')) }}">
                                         </div>
-                                        <div class="form-group col-md-5">
-                                            <label for="fecha_fin" class="font-weight-bold">F.Fin</label>
-                                            <input type="date" name="fecha_fin" id="fecha_fin" class="form-control" value="{{ request('fecha_fin', $fecha_fin ?? now()->format('Y-m-d')) }}">
+                                        <div class="form-group mx-2 col-5">
+                                            <label class="form-label" for="mes"><strong>F.Fin</strong></label>
+                                            <input class="form-control" type="date" name="fecha_fin" id="fecha_fin" value="{{ request('fecha_fin', $fecha_fin ?? now()->format('Y-m-d')) }}">
                                         </div>
-                                        <div class="col-md-2">
-                                            <button type="submit" class="btn btn-primary mb-2">Filtrar</button>
+                                        <div class="form-group mx-2 col-2">
+                                            <button type="submit" class="btn btn-primary">Filtrar</button>
                                         </div>
                                     </div>
                                 </div>
