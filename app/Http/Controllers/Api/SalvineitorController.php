bccc89b3d6d669008b19bb46cef5a88afb60ea7d
DesarrolloHawkins <122017573+DesarrolloHawkins@users.noreply.github.com>
2025-05-16 09:36:08 +0200

    Update SalvineitorController.php

--- app/Http/Controllers/Api/SalvineitorController.php
+++ app/Http/Controllers/Api/SalvineitorController.php
@@ -9,9 +9,24 @@ class SalvineitorController extends Controller
 {
     public function index()
     {
+
+        $now = Carbon::now(); // Usa la hora actual
+
+        $diaSemana = $now->dayOfWeek; // 0 (domingo) a 6 (sábado)
+        $hora = $now->hour;
+
+        if ($diaSemana >= 1 && $diaSemana <= 5 && $hora >= 7 && $hora <= 19) {
+            return response()->json([
+                'success' => 'OK'
+            ], 200);
+        } else {
+            return response()->json([
+                'success' => 'Fuera de horario permitido'
+            ], 400);
+        }
         # Retorna 200
-        return response()->json([
-            'sucess' => 'OK'
-        ], 200);
+        // return response()->json([
+        //     'sucess' => 'OK'
+        // ], 200);
     }
 }
