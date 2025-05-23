260a04deb21ca183fca584749c79f312674a03c5
HawkinsWeb <antonio@hawkins.es>
2025-05-23 13:10:29 +0200

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -51,7 +51,7 @@ class Kernel extends ConsoleKernel
         });
         $schedule->command('vacacioner:discount')->weeklyOn(6, '08:00');
         $schedule->command('kitdigital:avisar-contratos-antiguos')->dailyAt('08:00');
-        
+
         $schedule->call(function () {
 
             $files = Storage::files('public/excel');
