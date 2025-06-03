f4fa65f4d1171794644f24dedf435874ac5200eb
HawkinsWeb <antonio@hawkins.es>
2025-06-03 12:58:05 +0200

    Update ProcesarExcel.php

--- app/Console/Commands/ProcesarExcel.php
+++ app/Console/Commands/ProcesarExcel.php
@@ -367,6 +367,9 @@ class ProcesarExcel extends Command
                         $message = $movimiento['message'] ?? '';
                         $amount = $movimiento['amount'] ?? 0;
                         $received_date = $movimiento['received_date'] ? Carbon::parse($movimiento['received_date'])->format('Y-m-d') : '';
+                        if ($received_date < '2025-06-03') {
+                            continue;
+                        }
                         $saldo = $movimiento['saldo'] ?? 0;
 
                         $hashBase = $message . '|' . $amount . '|' . $received_date . '|' . $saldo;
