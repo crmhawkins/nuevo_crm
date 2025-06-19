2203d692314b9569d747722a0bd7263726e6041a
HawkinsWeb <128600888+crmhawkins@users.noreply.github.com>
2025-06-19 16:45:38 +0200

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -53,8 +53,8 @@ class Kernel extends ConsoleKernel
         //  $schedule->command('kitdigital:avisar-contratos-antiguos')->dailyAt('08:00');
 
         $schedule->command('Tesoreria:ProcesarExcel')->everyMinute();
-        $schedule->command('Whatsapp:GenerarMensajeCampania')->everyTenSeconds();
-        $schedule->command('Whatsapp:Enviar')->everyTenMinutes();
+        // $schedule->command('Whatsapp:GenerarMensajeCampania')->everyTenSeconds();
+        // $schedule->command('Whatsapp:Enviar')->everyTenMinutes();
 
 
         // $schedule->call(function () {
