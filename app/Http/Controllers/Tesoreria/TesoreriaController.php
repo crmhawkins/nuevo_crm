c5e24f18141fd11abaad4af0631eb7f3d1264c42
HawkinsWeb <128600888+crmhawkins@users.noreply.github.com>
2025-07-08 09:06:44 +0200

    Cambio tesoreria, cobrado parcial

--- app/Http/Controllers/Tesoreria/TesoreriaController.php
+++ app/Http/Controllers/Tesoreria/TesoreriaController.php
@@ -945,6 +945,9 @@ class TesoreriaController extends Controller
                 if($total == $invoice->total){
                     $invoice->invoice_status_id = 3;
                     $invoice->save();
+                } else {
+                    $invoice->invoice_status_id = 4;
+                    $invoice->save();
                 }
             }
 
